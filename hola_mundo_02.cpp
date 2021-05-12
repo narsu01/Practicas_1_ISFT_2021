5f47c0cc45e0640bcaa499b8b210d1f94d34dcba
narsu01 <lucas_fasciglione@hotmail.com>
2021-05-12 19:06:05 -0300

    cree hola_mundo_02

---
+++ hola_mundo_02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello " << name << endl;
+	cin.ignore();
+    cin.get();
+
+    return 0;
+}
